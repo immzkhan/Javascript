0bb5e0aca4eb564e8c1c9b2fbff98b01554b2991
Imrankhan <immzkhan532@gmail.com>
2023-03-10 23:07:55 +0530

    js files

---
+++ conditions.js
@@ -0,0 +1,13 @@
+//  if else 
+// switch case
+// ternary Operator / ternary condition 
+
+let rating = 3;
+
+if (rating == 5){
+    console.log(" 5 stars");
+}  else if ( rating == 4){
+    console.log(" 4 stars");
+}  else if ( rating == 3 ){
+    console.log("Average");
+}
