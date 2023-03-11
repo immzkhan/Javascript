c36bf5e41798b398c907fc1bfc66ccf085b90435
Imrankhan <immzkhan532@gmail.com>
2023-03-11 22:44:30 +0530

    conditions

--- conditions.js
+++ conditions.js
@@ -2,12 +2,33 @@
 // switch case
 // ternary Operator / ternary condition 
 
-let rating = 3;
+// let rating = 3;
 
-if (rating == 5){
-    console.log(" 5 stars");
-}  else if ( rating == 4){
-    console.log(" 4 stars");
-}  else if ( rating == 3 ){
-    console.log("Average");
-}
+// if (rating == 5){
+//     console.log(" 5 stars");
+// }  else if ( rating == 4){
+//     console.log(" 4 stars");
+// }  else if ( rating == 3 ){
+//     console.log("Average");
+// }
+
+// let user = 'admin';
+
+// switch(user){
+//    case 'admin':
+//       console.log("redirect the him to the admin page");
+// // break;
+// case 'mentor':
+//     console.log("redirect hin to mentor");
+//     break;
+
+//     default:
+//         console.log('I am guest');
+//   }
+
+
+// ternary
+
+let raining = false;
+
+raining ? console.log('its raining') : console.log('its not raing');
