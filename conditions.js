72edd2ad80e45abfc58337e7dc2fa289e50044a7
Imrankhan <immzkhan532@gmail.com>
2023-03-13 22:12:27 +0530

    loops & Date Math

--- conditions.js
+++ conditions.js
@@ -29,6 +29,81 @@
 
 // ternary
 
-let raining = false;
+// let raining = false;
 
-raining ? console.log('its raining') : console.log('its not raing');
+// raining ? console.log('its raining') : console.log('its not raing');
+
+// let userstatus = false;
+
+// userstatus ? console.log('Logout') : console.log('login');
+
+//  Arrryy 
+
+// let name = ["Anurag", "Imran","anirudh","harish","faiza","zain"];
+//  console.log(name[1
+//     ]);
+
+// Object
+
+// let bankdetails = {
+//     firstname : "Imran",
+//     lastname : 'Khan',
+//     mobile : 9888596522,
+//     balance : 152663,
+
+// }
+
+// to call All The things In It
+
+// console.log(bankdetails);
+
+// to get specific Item in Array
+
+// console.log(bankdetails.firstname);
+
+// Date & Math 
+
+// const imran = new Date();
+// console.log(imran);
+// console.log(imran.toDateString());
+// console.log(imran.getUTCDate(), imran.getFullYear());
+
+// const PI = Math.PI;
+
+// console.log(PI);
+
+// console.log(Math.round(1.2));
+// console.log(Math.floor(9.9));
+// console.log(Math.ceil(9.2));
+
+// console.log(Math.min(10,2,3,5,6,8,7));
+// console.log(Math.max(10,2,3,5,6,8,7));
+
+// console.log(Math.round(Math.random() *1));
+
+// power 
+// console.log(Math.pow(3,3));
+
+// log
+// console.log(Math.log(2));
+
+// trignometry
+// console.log(Math.sin(120));
+
+
+// let i = 7;
+// 0
+// do {
+//     console.log("value of i is :", i);
+//     i++;
+// } while (i <= 5);
+
+//  let j = 8;
+// while (j <= 7) {
+//     console.log(j,"is less than 7 ");
+//     j++;
+// }
+
+for(let i = 5; i <= 5; i++){
+    console.log('the value is :',i);
+}
