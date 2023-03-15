8fd8db3ca02e3bcf014f6b29ec17a0506fe5be44
Imrankhan <immzkhan532@gmail.com>
2023-03-15 23:00:35 +0530

    the recall

---
+++ new.js
@@ -0,0 +1,14 @@
+let admin = 'imra';
+
+switch(admin){
+    case 'imran':
+        console.log('welcome boss');
+        break;
+
+        case "teacher":
+            console.log('Shikshak Pranam');
+            break;
+
+            default:
+                console.log('login karlo bhai');
+}
