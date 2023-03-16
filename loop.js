505f503f62eb27f9126f57cf86b466439592430a
Imrankhan <immzkhan532@gmail.com>
2023-03-16 23:46:33 +0530

    LOOps

---
+++ loop.js
@@ -0,0 +1,9 @@
+let bankdetail = {
+    firstname : 'immz',
+    lastname: "khan",
+    mobile : 963215487,
+    Balance : "1 Crore",
+}
+
+console.log(bankdetail);
+console.log("my Balance is" ,bankdetail.Balance);
