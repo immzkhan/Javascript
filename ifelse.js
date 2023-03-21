5e1937043fb9288531e6298f4cd7da4b4190ba61
Imrankhan <immzkhan532@gmail.com>
2023-03-21 23:25:12 +0530

    trig

---
+++ ifelse.js
@@ -0,0 +1,45 @@
+// let admin = 1;
+
+// if(admin == 1){
+// console.log("Please be in Admin");
+// } else if ( admin == 0){
+//     console.log("Please Tell Admin To Give Assces");
+// }
+
+
+let rating = 3;
+
+if (rating == 5){
+    console.log(" 5 stars");
+}  else if ( rating == 4){
+    console.log(" 4 stars");
+}  else if ( rating == 3 ){
+    console.log("Average");
+}// Date & Math 
+
+// const imran = new Date();
+// console.log(imran);
+// console.log(imran.toDateString());
+// console.log(imran.getUTCDate(), imran.getFullYear());
+
+// const PI = Math.PI;
+
+// console.log(PI);
+
+// console.log(Math.round(1.2));
+// console.log(Math.floor(9.9));
+// console.log(Math.ceil(9.2));
+
+// console.log(Math.min(10,2,3,5,6,8,7));
+// console.log(Math.max(10,2,3,5,6,8,7));
+
+// console.log(Math.round(Math.random() *1));
+
+// power 
+// console.log(Math.pow(3,3));
+
+// log
+// console.log(Math.log(2));
+
+// trignometry
+// console.log(Math.sin(120));
