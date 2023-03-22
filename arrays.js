7892b549e447d0bc0f5cc734a70f81b0aefcaeb9
Imrankhan <immzkhan532@gmail.com>
2023-03-22 20:40:04 +0530

    arays

--- arrays.js
+++ arrays.js
@@ -17,7 +17,20 @@
 // Arrays Methods 
 
 let names = ["anurag","Imran","yaseen","imtiyaz",'usama'];
+// console.log(names);
+// console.log(names.length);
+// console.log(names[1]);
+
+// names[4] = 'Haseena';
+
+// console.log(names);
+// console.log(names[names.length - 1]);
+
+
+// New Way OF Declearing Arrya Is //  But Not Recomnded
+//  let namess = new Array('imran','kalid')
+// console.log(namess);
+
+names.push('vikaram');
+names.push('vikaram','pushpa','solii');
 console.log(names);
-console.log(names.length);
-console.log(names[1]);
-console.log(names[names.length - 2 ]);
