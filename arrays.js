d9cdfb28bd6a14df98b94350a8da33476f00e06d
Imrankhan <immzkhan532@gmail.com>
2023-03-22 19:53:45 +0530

    arrays

---
+++ arrays.js
@@ -0,0 +1,23 @@
+//   data types 
+//   variables 
+//   Date 
+//   Math
+//   loops 
+//   conditions
+//   op 
+//   indexing
+//   Object
+//   Arrays
+ 
+
+
+
+//  Arrays 
+
+// Arrays Methods 
+
+let names = ["anurag","Imran","yaseen","imtiyaz",'usama'];
+console.log(names);
+console.log(names.length);
+console.log(names[1]);
+console.log(names[names.length - 2 ]);
