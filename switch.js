8cd2a42c66eaa964aca4dbd6bcd780f5a7b50774
Imrankhan <immzkhan532@gmail.com>
2023-03-25 23:38:24 +0530

    switch

---
+++ switch.js
@@ -0,0 +1,14 @@
+
+let user = 'admin';
+
+switch(user){
+   case 'admin':
+      console.log("redirect the him to the admin page");
+break;
+case 'mentor':
+    console.log("redirect hin to mentor");
+    break;
+
+    default:
+   console.log('I am guest');
+       }
