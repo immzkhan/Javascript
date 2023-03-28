460704491127547efdecff8620691a867cbe329d
Imrankhan <immzkhan532@gmail.com>
2023-03-29 04:19:02 +0530

    loops

---
+++ break.js
@@ -0,0 +1,7 @@
+
+
+for( i = 0; i<= 5; i++){
+    if(i==3){
+        continue;
+    }console.log(i);
+}
---
+++ forof.js
@@ -0,0 +1,9 @@
+let fruit = ['imran','khan','hukki'];
+let upperfruit = [];
+
+for( let imran of fruit){
+    upperfruit.push(imran.toUpperCase());
+}
+// console.log(fruit);
+// console.log(upperfruit);
+console.log(imran);
