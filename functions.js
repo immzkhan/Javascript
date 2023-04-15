548f0cc2a9ede33d477bbada6f82453b097e4285
Imrankhan <immzkhan532@gmail.com>
2023-04-15 22:41:15 +0530

    functions

--- functions.js
+++ functions.js
@@ -1,1 +1,10 @@
-if {
+function URL(url,domain){
+    let con = 'http//';
+    let result = con + url + domain;
+    return result;
+}
+
+let inransite = URL('imrankhan','.com');
+console.log(inransite);  
+
+console.log(URL('imrankhan','.com'));
