8568f42f582cc2190c23956164434789fee61937
Imrankhan <immzkhan532@gmail.com>
2023-04-21 02:34:21 +0530

    chnages

--- functions.js
+++ functions.js
@@ -8,3 +8,5 @@ let inransite = URL('imrankhan','.com');
 console.log(inransite);  
 
 console.log(URL('imrankhan','.com')); //// Not Recomdeded
+let inransite = URL('imrankhan','.com');
+console.log(inransite); 
