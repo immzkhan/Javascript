d4fa04d1a23c3d47f4e389794b8cd84f2022b896
Imrankhan <immzkhan532@gmail.com>
2023-04-28 23:12:07 +0530

    g

---
+++ object.js
@@ -0,0 +1,21 @@
+ let userName = {
+    firstname: 'imran', 
+
+    lastname: 'khan',
+
+    role : 'admin',
+
+    loginCount : 25,
+
+
+ };
+
+ console.log(userName['role']);
+
+ userName.loginCount = 61;
+
+ console.log(userName);
+
+ for ( let x in userName){
+    console.log(userName[x]);
+ }
