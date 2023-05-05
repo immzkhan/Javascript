a3d387d0c15804345f6bdcbe108694e75d1ce088
Imrankhan <immzkhan532@gmail.com>
2023-05-05 22:11:09 +0530

    k

---
+++ obj.js
@@ -0,0 +1,44 @@
+//  const object = {
+//     roket : 1 ,
+//     imran : 2
+//  }  
+//   // we can loop as well 
+    
+//   for( k in object){
+//     console.log(k);
+//   }
+
+
+
+//   // part 1 
+
+//   const obj1 = {
+//     roket : 1 ,
+//     fuel: 2,
+//     config:{
+//         name:"mars"
+//     }
+//  }  
+
+//  obj1.fuel = 200
+//  obj1["year"] = 2000
+//  console.log(obj1);
+
+
+
+
+//  Part 2   Constructor 
+
+// const obj2 = new Object()
+
+// obj2.redbook = "red"
+// obj2.values = 200
+
+// console.log(obj2);
+
+
+
+// part 3
+
+
+
